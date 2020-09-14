5b0d4a3da6ca74ac3f30663594614b1229a10c47
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-14 20:21:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,8 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   index = 0 
   while array[index] do
-    
   if value_to_find == index
-  else 
+    return index
+  else return nil
   end 
 end
